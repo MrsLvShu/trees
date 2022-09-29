d8985532bce3ab74134d9a17af96dcf62d7f49a3
MrsLvShu <741722842@qq.com>
2022-09-29 09:27:20 +0800

    xg

--- trees/.cz.config.js
+++ trees/.cz.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // 可选类型
     types: [
-        { value: 'feat', name: 'feat:     新功能' },
+        { value: 'feat', name: 'feat:     新增功能' },
         { value: 'fix', name: 'fix:      修复' },
         { value: 'docs', name: 'docs:     文档变更' },
         { value: 'style', name: 'style:    代码格式(不影响代码运行的变动)' },
@@ -10,7 +10,7 @@ module.exports = {
         { value: 'test', name: 'test:     增加测试' },
         { value: 'chore', name: 'chore:    构建过程或辅助工具的变动' },
         { value: 'revert', name: 'revert:   回退' },
-        { value: 'build', name: 'build:    打包' }
+        { value: 'build', name: 'build:    打包' },
     ],
     // 消息步骤
     messages: {
